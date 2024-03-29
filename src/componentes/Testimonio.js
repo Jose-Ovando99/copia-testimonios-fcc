7b9af068bb109f5ff1f6c0ccc43156b0eb4dd5e4
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-03-29 11:24:11 -0600

    actualizar texto-testimonio de Testimonio.js

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -9,7 +9,7 @@ function Testimonio() {
       <div className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'>Sujeto 1, One</p>
         <p className='cargo-testimonio'>Ingeniero de software</p>
-        <p className='texto-testimonio'>Este texto lo voy a cambiar despues</p>
+        <p className='texto-testimonio'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at dolor ullamcorper nibh porta ornare sit amet non nunc. Donec ultricies interdum lacus gravida ornare. Phasellus aliquet rutrum sagittis. Vestibulum et augue vel velit congue commodo. Donec blandit urna ut vestibulum viverra. Mauris condimentum, ante nec euismod rutrum, risus urna convallis odio, et maximus diam metus in nisi.</p>
       </div>
     </div>
   );
