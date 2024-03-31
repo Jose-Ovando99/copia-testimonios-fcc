d723c2407bc013b001a71ccd15c945f35ed7a864
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-03-31 12:55:46 -0600

    Copia fcc finalizada, falta agregar responsividad

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import '../hojas-de-estilo/Testimonio.css'
 
-function Testimonio() {
+function Testimonio(props) {
+  let foto = 'foto de ' + props.imagen;
   return (
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio' 
-        src={require('../imagenes/testimonio-one.jpg')} 
-        alt='Foto de One' />
+        src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} 
+        alt={foto} />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Sujeto 1, One</p>
-        <p className='cargo-testimonio'>Ingeniero de software</p>
-        <p className='texto-testimonio'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at dolor ullamcorper nibh porta ornare sit amet non nunc. Donec ultricies interdum lacus gravida ornare. Phasellus aliquet rutrum sagittis. Vestibulum et augue vel velit congue commodo. Donec blandit urna ut vestibulum viverra. Mauris condimentum, ante nec euismod rutrum, risus urna convallis odio, et maximus diam metus in nisi.</p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
